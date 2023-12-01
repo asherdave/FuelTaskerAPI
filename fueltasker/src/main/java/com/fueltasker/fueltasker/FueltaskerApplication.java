22d743b5f725c0913ecde2243e8b8fedb843c5a2
Kenneth Ayade <ayadekenneth07@gmail.com>
2023-12-01 17:06:37 +0800

    origin

---
+++ fueltasker/src/main/java/com/fueltasker/fueltasker/FueltaskerApplication.java
@@ -0,0 +1,13 @@
+package com.fueltasker.fueltasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FueltaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FueltaskerApplication.class, args);
+	}
+
+}
